bdd3ee1f26ae8b9b4dd152b6c9ae8dab9feca9b9
Sterlites <101171175+Sterlites@users.noreply.github.com>
2024-10-09 16:45:03 +0530

    BF try6

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Exclude React DevTools in production
-      babel: {
-        plugins: process.env.NODE_ENV === 'production' 
-          ? ['babel-plugin-dev-expression']
-          : [],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(fileURLToPath(import.meta.url), './src'),
